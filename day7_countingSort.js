a42042ea6eb3507c572e326706aefd829ee0092b
kk062797 <kanishk423@gmail.com>
2024-12-26 01:54:34 +0530

    day7

--- day7_countingSort.js
+++ day7_countingSort.js
@@ -1,14 +1,20 @@
 const originalArray = [7, 9, 3, 0, 9, 5, 5, 1, 2, 1, 7];
 //index pos            0  1  2  3  4  5  6  7  8  9  10
 const len = originalArray.length;
-
-const countArray = new Array(len).fill(0);
+/*
+const arrayLike = {
+    length: 10,
+}
+const countArray = Array.from(arrayLike, () => 0)
+*/
+//OR
+const countArray = new Array(10).fill(0);
 
 for (let i = 0; i < len; i++) {
     currentVal = originalArray[i];
     countArray[currentVal]++;
 }
-console.log(countArray);
+//console.log(countArray);
 let sortedArray = [];
 for (let j = 0; j < countArray.length; j++) {
     let curr_Val = countArray[j];
